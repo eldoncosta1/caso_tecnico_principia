ea75b7d92a108aab6fa196c23e5fa948f0921b01
adhmattheus <mattheusadhonnaysf@hotmail.com>
2022-12-28 21:06:39 -0300

    escopo estático

--- src/InputFile.jsx
+++ src/InputFile.jsx
@@ -1,4 +1,3 @@
-
 import styles from './InputFile.module.css'
 import Papa from 'papaparse';
 import { useState } from 'react';
@@ -12,15 +11,61 @@ export function InputFile() {
     Papa.parse(e.target.files[0], {
       header: true,
       skipEmptyLines: true,
+      // dynamicTyping: true,
       complete: function (results) {
-        setFileData(results.data)
-      
+        setFileData(results.data.map(item => {
+          item.valor = Number(item.valor);
+          return item;
+        }));
       }
     })
   }
-  console.log(fileData);
+
+  // console.log(fileData);
+
+  const mesUm = fileData.filter((item) => item.mes === '2022-01')
+  // console.log(mesUm)
+
+  let total = 0;
+  const valorTotal = [...Array.from(new Set(mesUm.forEach(element => {
+    total += element.valor
+  })))]
+  console.log(`Valor total do mês: $${total}`);
+
+
+
+  const mesUmaberto = fileData.filter((item) => item.mes === '2022-01' && item.status === 'aberto')
+  // console.log(mesUmaberto)
+
+  let totalAberto = 0;
+  const valorTotalAberto = [...Array.from(new Set(mesUmaberto.forEach(element => {
+    totalAberto += element.valor
+  })))]
+  console.log(`Valor total em aberto do mês: $${totalAberto}`);
+
+  let inadiplenciaMesUm = (totalAberto / total).toFixed(3);
+  let inadiplenciaMesUmpercent = inadiplenciaMesUm * 100;
+
+  console.log(`Valor da inadiplência: ${inadiplenciaMesUm} ou ${inadiplenciaMesUmpercent}%`);
+
+  // const statusAberto = [...Array.from(new Set(mesUm.filter((item) => item.status === 'aberto')))]
+  // console.log(statusAberto);
+
+  // const statuspAgo = [...Array.from(new Set(mesUm.filter((item) => item.status === 'pago')))]
+  // console.log(statuspAgo);
+
+
+
+
+
+
+
+
+
+
 
   return (
+
     <div className={styles.container}>
       <input type="file"
         name="file"
@@ -28,5 +73,6 @@ export function InputFile() {
         onChange={onChangeHandler}
       />
     </div>
+
   )
 }
