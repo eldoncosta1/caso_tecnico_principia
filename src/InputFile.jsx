98c4f01939bc15319b67d94b4188bf3d7b72d3f4
adhmattheus <mattheusadhonnaysf@hotmail.com>
2023-01-03 20:50:53 -0300

    tratando dados e mes

--- src/InputFile.jsx
+++ src/InputFile.jsx
@@ -1,6 +1,9 @@
 import styles from './InputFile.module.css'
 import Papa from 'papaparse';
 import { useState } from 'react';
+import moment from 'moment/moment';
+import 'moment/locale/pt-br'
+moment.locale('pt-br');
 
 export function InputFile() {
   const [fileData, setFileData] = useState([]);
@@ -15,19 +18,75 @@ export function InputFile() {
       complete: function (results) {
         setFileData(results.data.map(item => {
           item.valor = Number(item.valor);
+          item.mes = moment(item.mes).format('MMMM YYYY')
           return item;
         }));
       }
     })
   }
 
-  const selectMonths = Object.values(fileData.reduce(function (months, item) {
-    months[item.mes] = months[item.mes] || []
-    months[item.mes].push(item)
-    return months
-  }, {}));
+  function groupBy(array, key) {
+    return array.reduce((acc, item) => {
+      if (!acc[item[key]]) acc[item[key]] = [];
+      acc[item[key]].push(item);
+      return acc;
+    }, {})
+  }
 
-  console.log(selectMonths)
+  console.log(fileData)
+
+  const meses = groupBy(fileData, "mes");
+  console.log(meses)
+
+  const keys = Object.keys(meses);
+  console.log(keys)
+
+  const extractByMonth = [];
+  const baddebtByMonth = [];
+
+  for (let i = 0; i < keys.length; i++) {
+
+    const keyI = keys[i];
+    let _valorAberto = 0;
+    let _valorTotal = 0;
+    let _valorPago = 0;
+
+    for (let j = 0; j <= i; j++) {
+      const keyJ = keys[j];
+      _valorAberto += meses[keyJ]
+        .filter((item) => item.status == "aberto")
+        .reduce((acc, item) => acc + parseFloat(item.valor), 0);
+      _valorPago += meses[keyJ]
+        .filter((item) => item.status == "pago")
+        .reduce((acc, item) => acc + parseFloat(item.valor), 0);
+      _valorTotal += meses[keyJ]
+        .reduce((acc, item) => acc + parseFloat(item.valor), 0);
+    }
+
+    extractByMonth.push({
+      mes: keyI,
+      valorPago: _valorPago,
+      valorAberto: _valorAberto,
+      valorTotal: _valorTotal
+    })
+
+    const badDebt = _valorAberto / _valorTotal;
+    baddebtByMonth.push({
+      mes: keyI,
+      inadiplencia: badDebt,
+    })
+  }
+  console.log(extractByMonth);
+  console.log(baddebtByMonth);
+
+
+
+  // const selectMonths = Object.values(fileData.reduce(function (months, item) {
+  //   months[item.mes] = months[item.mes] || []
+  //   months[item.mes].push(item)
+  //   return months
+  // }, {}));
+  // console.log(selectMonths)
 
   // let total = 0;
   // const valorTotal = [...Array.from(new Set(mesUm.forEach(element => {
@@ -35,8 +94,7 @@ export function InputFile() {
   // })))]
   // console.log(`Valor total do mês: $${total}`);
 
-  // const mesUmaberto = fileData.filter((item) => item.mes === '2022-01' && item.status === 'aberto')
-  // // console.log(mesUmaberto)
+
 
   // let totalAberto = 0;
   // const valorTotalAberto = [...Array.from(new Set(mesUmaberto.forEach(element => {
